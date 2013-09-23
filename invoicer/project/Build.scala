435a071725acb8ef6f7dc1468ee483da29bb7bcf
Johan Brook <johan.jvb@gmail.com>
2013-09-23 23:37:17 +0200

    Init with ebean enabled

--- invoicer/project/Build.scala
+++ invoicer/project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "invoicer"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appVersion      = "0.1"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
@@ -15,7 +15,8 @@ object ApplicationBuild extends Build {
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    // Add your own project settings here
+    ebeanEnabled := true
   )
 
 }
