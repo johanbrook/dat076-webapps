34cc8546276b20b721300c3228caae31007d912c
Andreas Rolen <adderollen@hotmail.com>
2013-09-30 18:13:23 +0200

    Finished client-controll

--- invoicer/app/controllers/Clients.java
+++ invoicer/app/controllers/Clients.java
@@ -9,10 +9,32 @@
 package controllers;
 
 import models.Client;
+import models.Invoice;
+import models.User;
 import play.data.Form;
 import play.mvc.Controller;
+import play.mvc.Result;
 
 public class Clients extends Controller {
 
 	public static Form<Client> newForm = Form.form(Client.class);
+	
+	public static Result index() {
+		return ok(views.html.clients.index.render(Client.find.all(), newForm));
+	}
+	
+
+	public static Result create() {
+		Form<Client> filledForm = newForm.bindFromRequest();
+		
+		if(filledForm.hasErrors()) {
+			return badRequest(views.html.clients.index.render(Client.find.all(), filledForm));
+		}
+		else {
+			Client client = filledForm.get();
+			client.save();
+			return redirect(controllers.routes.Clients.index());
+		}
+		
+	}
 }
