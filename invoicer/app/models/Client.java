05b0f931160fc55538e9e59fda4aa484895831c4
Andreas Rolen <adderollen@hotmail.com>
2013-09-30 14:11:04 +0200

    Add Client model

---
+++ invoicer/app/models/Client.java
@@ -0,0 +1,52 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+import play.data.validation.Constraints.Required;
+import play.db.ebean.Model;
+
+
+public class Client extends Model {
+	
+	@Id
+	private long id;
+	@Required
+	@Column(nullable=false) // Should this use unique=true?
+	public String name;
+	@Required
+	@Column(nullable=false)
+	public String postalCode;
+	@Required
+	@Column(nullable=false)
+	public String country;
+	@Required
+	@Column(nullable=false, unique=true)
+	public String orgNumber;
+	@Required
+	@Column(nullable=false)
+	public String contactPerson;
+	
+	public Client(String name, String postalCode, String country, String orgNumber, String contactPerson) {
+		this.name = name;
+		this.postalCode = postalCode;
+		this.country = country;
+		this.orgNumber = orgNumber;
+		this.contactPerson = contactPerson;
+	}
+	
+	public static Client create(String name, String postalCode, String country, String orgNumber, String contactPerson) {
+		return create(new Client(name, postalCode, country, orgNumber, contactPerson));
+		
+	}
+	
+	public static Client create(Client client){ 
+		client.save();
+		return client;
+	}
+	
+	public Long getId() {
+		return this.id;
+	}
+
+}
