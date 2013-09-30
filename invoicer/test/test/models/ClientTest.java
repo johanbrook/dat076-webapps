387120c7dbd1cb2df2500a1e189876f8b3d89dc6
Andreas Rolen <adderollen@hotmail.com>
2013-09-30 15:20:46 +0200

    Add ClientTest

---
+++ invoicer/test/test/models/ClientTest.java
@@ -0,0 +1,42 @@
+package test.models;
+
+import static org.junit.Assert.*;
+
+import models.Client;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClientTest {
+
+	private Client client;
+	
+	@Before
+	public void setUp() throws Exception {
+		client = this.client.find.where().eq("orgNumber", "556479-5598").findUnique();
+	}
+
+	@Test
+	public void testRetriveClient() {
+		assertNotNull(this.client);
+		assertEquals("556479-5598", this.client.orgNumber);
+	}
+	
+	@Test
+	public void testCreateClient() {
+		Client carlsberg = new Client("carlsberg", "559823-5523");
+		
+		assertNotNull(carlsberg);
+		assertNotNull(carlsberg.getId());
+		assertEquals("559823-5523", carlsberg.orgNumber);
+	}
+	
+	@Test
+	public void testDeleteClient() {
+		this.client.delete();
+		
+		Client tmp = Client.find.where().eq("orgNumber", "556479-5598").findUnique();
+		assertNull(tmp);
+	}
+
+}
