778d42fbd7b647c300aebd0d5f645498bcf071a0
Johan Brook <johan.jvb@gmail.com>
2013-10-01 15:51:52 +0200

    Fix PersistenceException: not adding @MappedSuperclass to AbstractModel

--- invoicer/app/models/AbstractModel.java
+++ invoicer/app/models/AbstractModel.java
@@ -12,9 +12,11 @@
 package models;
 
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
 import play.db.ebean.Model;
 
+@MappedSuperclass
 public abstract class AbstractModel extends Model {
 
 	@Id
