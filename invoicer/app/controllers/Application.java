ce3e83e017fd81bf056aee373ea3053fadfe0c29
Robin Andersson <k.robin.andersson@gmail.com>
2013-10-02 19:00:25 +0200

    Add javadoc and comments

--- invoicer/app/controllers/Application.java
+++ invoicer/app/controllers/Application.java
@@ -7,39 +7,62 @@ import play.mvc.*;
 
 public class Application extends Controller {
   
-	@Security.Authenticated(Secured.class)
+	// All calls to this method is authenticated by the Secured class
+	@Security.Authenticated(Secured.class)	
     public static Result index() {
         return redirect(controllers.routes.Invoices.index());
     }
     
+	/**
+	 * (Action called from GET to /login)
+	 * Render the login screen
+	 * @return A 200 OK simple result
+	 */
     public static Result login() {
-    	return ok(views.html.login.render(Form.form(Login.class)));
+    	
+    	// Send the LoginForm class to the form
+    	return ok(views.html.login.render(Form.form(LoginForm.class)));
     }
     
+    /**
+     * (Action called from POST to /login)
+     * Authenticates the login request. Sets the Session attribute 'username'.  
+     * 
+     * @return Call to index method,
+     * else badRequest status code and rerendering of the login form
+     */
     public static Result authenticate() {
-    	Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
+    	
+    	// The login form wraps the Login class (inner class below)
+    	Form<LoginForm> loginForm = Form.form(LoginForm.class).bindFromRequest();
+    	
     	if(loginForm.hasErrors()) {
     		return badRequest(views.html.login.render(loginForm));
     	}
+    	
+    	// Clear existing session and add logged in user's username
     	session().clear();
-    	session("user", loginForm.get().username);
+    	session("username", loginForm.get().username);
+    	
     	return index();
     }
     
     /**
-     * Inner class for handling user login
+     * Inner class for handling user login (sent as parameter to the login form)
      * 
      * @author Robin
      */
-    public static class Login {
+    public static class LoginForm {
     	public String username;
     	public String password;
     	
     	/**
-    	 * Validates the username and password against existing users in database
+    	 * Ad-hoc validation, for username and password
     	 * @return Error message if username or password didn't match, else null
     	 */
     	public String validate() {
+    		
+    		// Call the User objects authentication
     		if(User.authenticateUser(username, password) == null) {
     			return "Wrong username or password";
     		}
--- invoicer/app/controllers/Secured.java
+++ invoicer/app/controllers/Secured.java
@@ -9,14 +9,23 @@ import play.mvc.Security.Authenticator;
 
 import models.*;
 
-
 public class Secured extends Authenticator {
 	
+	/*
+	 * If this method returns a value then the authenticator considers the user
+	 * to be logged in and lets the request proceed.
+	 * 
+	 * If the method returns null then the authenticator will block the request
+	 * and instead invoke onUnathorized
+	 */
 	@Override
     public String getUsername(Context ctx) {
         return ctx.session().get("username");
     }
-
+	
+	/*
+	 * Redirect to login screen
+	 */
     @Override
     public Result onUnauthorized(Context ctx) {
         return redirect(routes.Application.login());
