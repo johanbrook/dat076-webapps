f2d9939fd7449d1923f9fd512894b34cd882aeec
Henrik Andersson <heenrik@hotmail.com>
2013-10-03 09:08:05 +0200

    Add more test methods for BankAccountTest

--- invoicer/test/test/models/BankAccountTest.java
+++ invoicer/test/test/models/BankAccountTest.java
@@ -2,11 +2,11 @@ package test.models;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-
+import static org.junit.Assert.assertNull;
 import org.junit.Before;
 import org.junit.Test;
-
 import models.BankAccount;
+import models.BankAccount.AccountType;
 
 public class BankAccountTest extends BaseModelTest{
 	
@@ -22,6 +22,23 @@ public class BankAccountTest extends BaseModelTest{
 	public void testRetrieveBankAccount() {
 		assertNotNull(this.bankAccount);
 		assertEquals("1234-5678", this.bankAccount.accountNumber);
+	}
+	
+	@Test
+	public void testCreateBankAccount() {
+		BankAccount ba = new BankAccount("112233-4", AccountType.PG);
+		ba.save();
+		
+		assertNotNull(ba);
+		assertNotNull(ba.id);
+		assertEquals(AccountType.PG, ba.accountType);
+	}
+	
+	@Test
+	public void testDeleteBankAccount() {
+		this.bankAccount.delete();
+		BankAccount newBA = BankAccount.find.where().eq("accountNumber", "1234-5678").findUnique();
+		assertNull(newBA);
 	}
 
 }
