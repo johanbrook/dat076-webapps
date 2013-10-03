fac877b5c214f831c8ee247cebda006c8083043f
Robin Andersson <k.robin.andersson@gmail.com>
2013-10-03 18:56:16 +0200

    Move login console message from Secured class to Session class

--- invoicer/app/controllers/Secured.java
+++ invoicer/app/controllers/Secured.java
@@ -21,7 +21,6 @@ public class Secured extends Authenticator {
 	 */
 	@Override
     public String getUsername(Context ctx) {
-		Logger.info("*** User '" + ctx.session().get("username") + "' logged in ***");
         return ctx.session().get("username");
     }
 	
