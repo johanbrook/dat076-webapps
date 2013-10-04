e574fce8ea4834961daa9aa3bf4603d92b040fb8
Andreas Rolen <adderollen@hotmail.com>
2013-10-04 13:42:17 +0200

    Started add test in Clientstest

--- invoicer/test/test/controllers/ClientsTest.java
+++ invoicer/test/test/controllers/ClientsTest.java
@@ -2,14 +2,22 @@ package test.controllers;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import models.Client;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import com.avaje.ebean.Ebean;
+
 import play.data.Form;
+import play.libs.Yaml;
 import play.mvc.Result;
 import play.test.Helpers;
+
 
 public class ClientsTest extends Helpers {
 	
@@ -17,12 +25,17 @@ public class ClientsTest extends Helpers {
 
 	@Before
 	public void setUp() throws Exception {
-		newForm = Form.form(Client.class);
+		start(fakeApplication(inMemoryDatabase(), fakeGlobal()));
+		Ebean.save((List) Yaml.load("test-data.yml"));
 	}
 
 	@Test
 	public void testIndex() {
-		Result result = callAction(controllers.routes.ref.Clients.index());
+		Map<String,String> data = new HashMap<String,String>();
+		data.put("name", "testName");
+		data.put("orgNumber", "666666-6666");
+		
+		Result result = callAction(controllers.routes.ref.Clients.index(), fakeRequest().withFormUrlEncodedBody(data));
 			    assertEquals(status(result), OK);
 			    assertEquals(contentType(result), "text/html");
 			    assertEquals(charset(result), "utf-8");
