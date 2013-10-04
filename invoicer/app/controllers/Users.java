13f8934933300c62399c919c25adb5297e48674d
Robin Andersson <k.robin.andersson@gmail.com>
2013-10-04 16:21:23 +0200

    Create skeleton for User class (for creating new Users)

---
+++ invoicer/app/controllers/Users.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package controllers;
+
+import models.Client;
+import models.User;
+import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * @author Robin
+ *
+ */
+public class Users extends Controller {
+	
+	public static Form<User> form = Form.form(User.class);
+	
+	
+	/**
+	 * (Action called from GET to /createuser)
+	 * 
+	 * 
+	 * @return
+	 */
+	public static Result index() {
+		return null;
+	}
+	
+	/**
+	 * (Action called from POST to /createuser)
+	 * 
+	 * @return
+	 */
+	public static Result create() {
+		/*
+		Form<User> filledForm = form.bindFromRequest();
+		
+		if(filledForm.hasErrors()) {
+			return badRequest(views.html.views.user.render());
+		}
+		
+		User user = filledForm.get();
+		user.save();
+		*/
+		
+		return null;
+		
+	}
+
+}
