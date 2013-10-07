82f814a9119dd1157faa0ab8db29d506f1949f9f
Henrik Andersson <heenrik@hotmail.com>
2013-10-07 21:18:29 +0200

    Add test for BankAccounts

---
+++ invoicer/test/test/controllers/BankAccountsTest.java
@@ -0,0 +1,53 @@
+
+package test.controllers;
+
+import static org.junit.Assert.*;
+
+
+import models.BankAccount;
+
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableMap;
+
+import play.mvc.*;
+import static play.test.Helpers.*;
+import play.test.*;
+
+import test.BaseTest;
+
+public class BankAccountsTest extends BaseTest {
+
+	
+	@Test
+	public void testIndex() {
+		Result index = callAction(controllers.routes.ref.BankAccounts.index());
+		
+		assertEquals(OK, status(index));
+	}
+	
+	@Test
+	public void testShow() {
+		Long existingId = BankAccount.find.all().get(0).id;
+		
+		Result show = callAction(controllers.routes.ref.BankAccounts.show(existingId));
+		
+		assertEquals(OK, status(show));
+	}
+	
+	@Test
+	public void testCreate() {
+		
+		
+	}
+	
+	@Test
+	public void testUpdate() {
+		
+	}
+	
+	@Test
+	public void testDestroy() {
+		
+	}
+}
