5e367bb419345a86a5f3dc52b81e07751e3ce0bd
Johan Brook <johan.jvb@gmail.com>
2013-10-08 18:23:17 +0200

    Increment the indicator of total number of invoices when created

--- invoicer/app/views/invoices/create.scala.js
+++ invoicer/app/views/invoices/create.scala.js
@@ -2,4 +2,12 @@
 
 var item = "@views.html.invoices.item.render(invoice)";
 
+// TODO: put the rendered item into the correct spot in 
+// the list.
 $(".invoice-list").prepend(item);
+
+incrementTotal($("#invoices-total"));
+
+function incrementTotal(element) {
+	element.text(parseInt(element.text()) + 1);
+}
