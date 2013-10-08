bcb668dd7bda8e4da1ed6520469d3553699e72ce
Johan Brook <johan.jvb@gmail.com>
2013-10-08 21:18:23 +0200

    Add fancy animation when adding an invoice

--- invoicer/app/views/invoices/create.scala.js
+++ invoicer/app/views/invoices/create.scala.js
@@ -4,7 +4,7 @@ var item = "@views.html.invoices.item.render(invoice)";
 
 // TODO: put the rendered item into the correct spot in 
 // the list.
-$(".invoice-list").prepend(item);
+$(".invoice-list").prepend(item).find("li:first").addClass("added");
 
 incrementTotal($("#invoices-total"));
 
