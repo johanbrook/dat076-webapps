5da9f55d89ad6d4e835298f7b26c84bb60d65829
Henrik Andersson <heenrik@hotmail.com>
2013-10-09 21:17:53 +0200

    Refactor validation in BankAccounts

--- invoicer/app/controllers/BankAccounts.java
+++ invoicer/app/controllers/BankAccounts.java
@@ -1,4 +1,5 @@
 package controllers;
+
 import controllers.Application.Responder;
 import play.data.Form;
 import play.libs.Json;
@@ -21,50 +22,48 @@ public class BankAccounts extends Application {
 
 	public static Result index() {
 
-
 		return ok(index.render(BankAccount.find.all(), form));
 
 	}
 
 	public static Result show(Long id) {
 
-
 		return ok(show.render(BankAccount.find.byId(id)));
 
 	}
 
-	public static Result create(){
-final Form<BankAccount> filledForm = form.bindFromRequest();
-		
-		if(filledForm.hasErrors()) {
-			
+	public static Result create() {
+		final Form<BankAccount> filledForm = form.bindFromRequest();
+
+		if (filledForm.hasErrors()) {
+
 			return respondTo(new Responder() {
-				
+
 				@Override
 				public Result json() {
 					return badRequest();
 				}
-				
+
 				@Override
 				public Result html() {
 					flash("error", "There were errors in your form.");
-					return badRequest(index.render(BankAccount.find.all(), filledForm));
+					return badRequest(index.render(BankAccount.find.all(),
+							filledForm));
 				}
 			});
-		}
-		else {
+		} else {
 			final BankAccount ba = filledForm.get();
-			
+
 			ba.save();
-			
+
 			return respondTo(new Responder() {
-				
+
 				@Override
 				public Result json() {
 					setLocationHeader(ba);
 					return created(Json.toJson(ba));
 				}
-				
+
 				@Override
 				public Result html() {
 					flash("success", "Bank account was created!");
@@ -86,30 +85,35 @@ final Form<BankAccount> filledForm = form.bindFromRequest();
 		BankAccount bankAccount = BankAccount.find.byId(id);
 		Form<BankAccount> filledForm = form.bindFromRequest();
 
-		if(filledForm.hasErrors()) {
+		if (filledForm.hasErrors()) {
 			return badRequest(edit.render(bankAccount, filledForm));
 		}
-
-		bankAccount.accountType = filledForm.get().accountType;
+		
 		bankAccount.bank = filledForm.get().bank;
 		bankAccount.iban = filledForm.get().iban;
 		bankAccount.bic = filledForm.get().bic;
-		setAccountNumber(bankAccount, filledForm);
 		
+		// Validates here because annotation values in model can not change
+		// during runtime
+		if (validate(filledForm)) {
+			bankAccount.accountNumber = filledForm.get().accountNumber;
+			bankAccount.accountType = filledForm.get().accountType;
+		}
+		
+
 		bankAccount.update(id);
 
 		return goHome();
 	}
-	
-	public static Result destroy(Long id){
+
+	public static Result destroy(Long id) {
 		BankAccount bankAccount = BankAccount.find.byId(id);
-		
-		if(bankAccount != null) {
+
+		if (bankAccount != null) {
 			bankAccount.delete();
 			flash("success", "The bankAccount was deleted.");
 			return goHome();
-		}
-		else {
+		} else {
 			return notFound(show.render(bankAccount));
 		}
 	}
@@ -117,27 +121,31 @@ final Form<BankAccount> filledForm = form.bindFromRequest();
 	private static Result goHome() {
 		return redirect(controllers.routes.BankAccounts.index());
 	}
-	
-	private static void setAccountNumber(BankAccount bankAccount, Form<BankAccount> filledForm ){
-		AccountType aType = bankAccount.accountType;
+
+	// Method to validate accountNumber
+	private static boolean validate(Form<BankAccount> filledForm) {
 		String aNumber = filledForm.get().accountNumber;
-		
-		switch(aType){
-		 case PG: if(aNumber.matches("[0-9]{1,6}(-[0-9]{1})")){
-			 bankAccount.accountNumber = aNumber;
-		 }
-        break;
-		 case BG: if(aNumber.matches("[0-9]{4}-[0-9]{4}")){
-			 bankAccount.accountNumber = aNumber;
-		 }
-		 break;
-		 case BUSINESSACCOUNT: if(aNumber.matches("[0-9]{4}(-[0-9]{1})[0-9]{0,9}(-[0-9]){0,1}")){
-			 bankAccount.accountNumber = aNumber;
-		 }
-		 break;
-		 default: 
-        break;
+
+		switch (filledForm.get().accountType) {
+		case PG:
+			if (aNumber.matches("[0-9]{1,6}-[0-9]{1}")) {
+				return true;
+			}
+			break;
+		case BG:
+			if (aNumber.matches("[0-9]{4}-[0-9]{4}")) {
+				return true;
+			}
+			break;
+		case BUSINESSACCOUNT:
+			if (aNumber.matches("[0-9]{4}(-[0-9]{1})[0-9]{0,9}(-[0-9]){0,1}")) {
+				return true;
+			}
+			break;
+		default:
+			return false;
 		}
+		return false;
 	}
 
 }
