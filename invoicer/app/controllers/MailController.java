0e88dfa967bf565d5c70cee6bd81fdfc129a7976
Andreas Rolen <adderollen@hotmail.com>
2013-10-10 10:14:42 +0200

    Added method for sending all invoices to a client

--- invoicer/app/controllers/MailController.java
+++ invoicer/app/controllers/MailController.java
@@ -7,6 +7,8 @@
  */
 package controllers;
 
+import java.util.List;
+
 import javax.mail.MessagingException;
 import javax.mail.internet.AddressException;
 
@@ -26,11 +28,15 @@ public class MailController {
 		message.append("Invoice name: " + invoice.title + "\n" + "Due date: "
 				+ invoice.dueDate + "\n");
 		// TODO Update model with this
-		//message.append("To bank account type: " + invoice.owner.bankAccounts[0].type + "\n" + "Bank: " + invoice.owner.bankAccount[0].bank + "\n" + "Account number: " + invoice.owner.bankAccount[0].accountNumber)
-		//message.append("Sum to pay: " + invoice.amount);
-		
+		// message.append("To bank account type: " +
+		// invoice.owner.bankAccounts[0].type + "\n" + "Bank: " +
+		// invoice.owner.bankAccount[0].bank + "\n" + "Account number: " +
+		// invoice.owner.bankAccount[0].accountNumber)
+		// message.append("Sum to pay: " + invoice.amount);
+
 		try {
-			GoogleMail.send(userName, pw, client.email, "You got a new invoice", message.toString());
+			GoogleMail.send(userName, pw, client.email,
+					"You got a new invoice", message.toString());
 		} catch (AddressException e) {
 			Logger.info("Email address parse failed");
 			e.printStackTrace();
@@ -38,6 +44,34 @@ public class MailController {
 			Logger.info("Connection is dead");
 			e.printStackTrace();
 		}
+	}
 
+	public static void sendAllInvoices(String userName, String pw, Client client, List<Invoice> invoiceList) {
+		StringBuilder message = new StringBuilder("Hi "  + client.name + "\n");
+		if(invoiceList.size() > 0) {
+			message.append("Here is all your invoices to pay" + "\n");
+			for(Invoice i : invoiceList) {
+				message.append("Invoice name: " + i.title + "\n" + "Due date: "
+						+ i.dueDate + "\n");
+				// TODO Update model with this
+				// message.append("To bank account type: " +
+				// i.owner.bankAccounts[0].type + "\n" + "Bank: " +
+				// i.owner.bankAccount[0].bank + "\n" + "Account number: " +
+				// i.owner.bankAccount[0].accountNumber)
+				// message.append("Sum to pay: " + i.amount);
+			}
+		} else {
+			message.append("Congratulations you don't have any invoices to pay");
+		}
+		try {
+			GoogleMail.send(userName, pw, client.email,
+					"All your invoices", message.toString());
+		} catch (AddressException e) {
+			Logger.info("Email address parse failed");
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			Logger.info("Connection is dead");
+			e.printStackTrace();
+		}
 	}
 }
