1f0c63c3f0a690c9e96db501028be27c4704b7d3
Robin Andersson <k.robin.andersson@gmail.com>
2013-10-10 15:36:16 +0200

    Add update function to Users controller

--- invoicer/app/controllers/Users.java
+++ invoicer/app/controllers/Users.java
@@ -5,6 +5,7 @@ package controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import models.Client;
 import models.Invoice;
@@ -110,6 +111,80 @@ public class Users extends Application {
 		form = form.fill(Session.getCurrentUser());
 		
 		return ok(views.html.users.edit.render(form));
+	}
+	
+	
+	/**
+	 * (Action called from POST to /user/edit)
+	 * @return
+	 */
+	public static Result update() {
+		
+		Form<User> filledForm = form.bindFromRequest();
+		
+		Logger.info("Form errors " + filledForm.errors());
+		
+        // Check password
+        if(!filledForm.field("oldPassword").valueOr("").isEmpty()) {
+        	
+        	if(!BCrypt.checkpw(filledForm.field("oldPassword").value(), Session.getCurrentUser().password)) {
+        		filledForm.reject("oldPassword", "Incorrect password");
+        	}
+        	
+        	else if(!filledForm.field("newPassword").valueOr("").equals(filledForm.field("repeatNewPassword").value())) {
+                filledForm.reject("repeatNewPassword", "Passwords don't match");
+            }
+        	
+        	else {
+        	
+	        	Map<String, String> newForm = filledForm.data();
+	        	
+	        	newForm.put("password", filledForm.field("newPassword").value());
+	        	
+	        	filledForm = filledForm.bind(newForm);
+        	}
+        }
+        
+        // Observe that username not able to edit (for now), so these are redundant
+        /*
+    	String username = filledForm.field("username").valueOr("");
+    	
+    	if(username.equals("admin") || username.equals("guest")) {
+            filledForm.reject("username", "'admin' and 'guest' are reserved usernames");
+        
+        } else if(!username.equals(Session.getCurrentUser().username) &&
+        		User.find.where().eq("username", username).findUnique() != null) {
+    		filledForm.reject("username", "Username is taken!");
+    	}
+    	*/
+		
+		if(filledForm.hasErrors()) {
+			
+			Logger.info("Form errors " + filledForm.errors());
+			return badRequest(edit.render(filledForm));
+		}
+		
+		// Form valid, create user
+		User user = filledForm.get();
+		
+		// TODO: Do this somewhere else, conventions?
+		// set country to null if no country was chosen
+		if(filledForm.field("country").value().equals("default")) {
+			user.country = null;
+		}
+		
+		user.id = Session.getCurrentUser().id;
+		
+		// Hash the password with jBCrypt and save to database
+		user.password = BCrypt.hashpw(user.password, BCrypt.gensalt());
+		
+		Logger.info(user.id + " " + user.username + " " + user.address + " " + user.country + " " + user.name + " " + user.organizationNumber);
+		user.update();
+		
+		session("userId", String.valueOf(user.id));
+		Logger.info("*** User '" + user.username + "' edited ***");
+		
+		return redirect(controllers.routes.Users.show());
 	}
 
 }
