c47371e459a69870d2e819dbbfdcc02cb28ff53d
Andreas Rolen <adderollen@hotmail.com>
2013-10-15 17:35:28 +0200

    Added bankinfo in mails

--- invoicer/app/controllers/MailController.java
+++ invoicer/app/controllers/MailController.java
@@ -15,6 +15,7 @@ import javax.mail.internet.AddressException;
 import play.Logger;
 
 import util.GoogleMail;
+import models.BankAccount;
 import models.Client;
 import models.Invoice;
 
@@ -26,16 +27,17 @@ public class MailController {
 		message.append("You got a new invoice to pay" + "\n");
 		message.append("Invoice name: " + invoice.title + "\n" + "Due date: "
 				+ invoice.dueDate + "\n");
-		// TODO Update model with this
-		// message.append("To bank account type: " +
-		// invoice.owner.bankAccounts[0].type + "\n" + "Bank: " +
-		// invoice.owner.bankAccount[0].bank + "\n" + "Account number: " +
-		// invoice.owner.bankAccount[0].accountNumber)
+		message.append("Account type: "
+				+ invoice.bankAccount.accountType.getName() + "\nBank: "
+				+ invoice.bankAccount.bank + "\nAccount nr: "
+				+ invoice.bankAccount.accountNumber + "\nBIC: "
+				+ invoice.bankAccount.bic + "\nIBAN: " + invoice.bankAccount.iban
+				+ "\n");
 		message.append("Sum to pay: " + invoice.totalRate);
 
 		try {
-			GoogleMail.send(invoice.client.email,
-					"You got a new invoice", message.toString());
+			GoogleMail.send(invoice.client.email, "You got a new invoice",
+					message.toString());
 		} catch (AddressException e) {
 			Logger.info("Email address parse failed");
 			e.printStackTrace();
@@ -47,25 +49,27 @@ public class MailController {
 
 	public static void sendAllInvoices(List<Invoice> invoiceList) {
 		StringBuilder message = new StringBuilder();
-		if(invoiceList.size() > 0) {
+		if (invoiceList.size() > 0) {
 			message.append("Hi " + invoiceList.get(0).client.name + "! \n\n");
-			message.append("Here is all your invoices to pay from the user " + invoiceList.get(0).owner.name + "\n\n");
-			for(Invoice i : invoiceList) {
+			message.append("Here is all your invoices to pay from the user "
+					+ invoiceList.get(0).owner.name + "\n\n--------------------------------- \n");
+			for (Invoice i : invoiceList) {
 				message.append("Invoice name: " + i.title + "\n" + "Due date: "
-						+ i.dueDate + "\n");
-				// TODO Update model with this
-				// message.append("To bank account type: " +
-				// i.owner.bankAccounts[0].type + "\n" + "Bank: " +
-				// i.owner.bankAccount[0].bank + "\n" + "Account number: " +
-				// i.owner.bankAccount[0].accountNumber)
+						+ i.dueDate + "\n\n");
+				message.append("Account type: "
+						+ i.bankAccount.accountType.getName() + "\nBank: "
+						+ i.bankAccount.bank + "\nAccount nr: "
+						+ i.bankAccount.accountNumber + "\nBIC: "
+						+ i.bankAccount.bic + "\nIBAN: " + i.bankAccount.iban
+						+ "\n");
 				message.append("Sum to pay: " + i.totalRate);
-				message.append("\n" + "\n");
+				message.append("\n--------------------------------- \n");
 			}
 		} else {
 			message.append("Congratulations you don't have any invoices to pay");
 		}
 		try {
-			GoogleMail.send(invoiceList.get(0).client.email, 
+			GoogleMail.send(invoiceList.get(0).client.email,
 					"All your invoices", message.toString());
 		} catch (AddressException e) {
 			Logger.info("Email address parse failed");
@@ -75,14 +79,16 @@ public class MailController {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void sendReminder(Invoice invoice) {
-		StringBuilder message = new StringBuilder("Hi " + invoice.client.name + "\n");
-		message.append("Remember that you got an unpayed invoice to pay due to " + invoice.dueDate + "\n");
+		StringBuilder message = new StringBuilder("Hi " + invoice.client.name
+				+ "\n");
+		message.append("Remember that you got an unpayed invoice (" + invoice.title + ") to pay due to "
+				+ invoice.dueDate + "\n");
 		message.append("Amount to pay: " + invoice.totalRate);
 		try {
-			GoogleMail.send(invoice.client.email, 
-					"Reminder: Invoice to pay", message.toString());
+			GoogleMail.send(invoice.client.email, "Reminder: Invoice to pay",
+					message.toString());
 		} catch (AddressException e) {
 			Logger.info("Email address parse failed");
 			e.printStackTrace();
