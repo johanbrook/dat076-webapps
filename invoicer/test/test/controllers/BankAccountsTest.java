55c95625bbfffe7761226692cc485529a77921d5
Henrik Andersson <heenrik@hotmail.com>
2013-10-17 16:53:09 +0200

    Update tests for create and update in BankAccountsTest

--- invoicer/test/test/controllers/BankAccountsTest.java
+++ invoicer/test/test/controllers/BankAccountsTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 
 
 import models.BankAccount;
-
 import org.junit.Test;
 
 import com.google.common.collect.ImmutableMap;
@@ -17,51 +16,71 @@ import test.BaseTest;
 
 public class BankAccountsTest extends BaseTest {
 
-	
+
 	@Test
 	public void testIndex() {
 		Result index = callAction(controllers.routes.ref.BankAccounts.index());
-		
+
 		assertEquals(303, status(index));
 	}
-	
+
 	@Test
 	public void testShow() {
 		Long existingId = BankAccount.find.all().get(0).id;
-		
+
 		Result show = callAction(controllers.routes.ref.BankAccounts.show(existingId));
-		
+
 		assertEquals(303, status(show));
 	}
-	
+
 	@Test
 	public void testCreate() {
 		Result create = callAction(
 				controllers.routes.ref.BankAccounts.create(),
 				fakeRequest()
-				.withSession("userId", "1")
+				.withSession("userId", super.userId)
 				.withFormUrlEncodedBody(ImmutableMap.of(
-						"accountNumber", "1234-5678",
-						"accountType", "PG"
-				))
-			);
-		
+						"accountNumber", "1234-5555",
+						"accountType", "BG",
+						"iban", "SE0004563215864902",
+						"bic", "SWEDSESS"
+						))
+				);
+
 		assertEquals(303, status(create));
-		assertEquals("/bankaccounts", header("Location", create));
-		
-		BankAccount created = BankAccount.find.where().eq("accountNumber", "1234-5678").findUnique();
-		
+		assertEquals("/accounts", header("Location", create));
+
+		BankAccount created = BankAccount.find.where().eq("accountNumber", "1234-5555").findUnique();
+
 		assertNotNull(created);
-		
+
+
 	}
-	
+
 	@Test
 	public void testUpdate() {
-		
+		BankAccount existing = BankAccount.find.all().get(0);
+
+		Result update = callAction(
+				controllers.routes.ref.BankAccounts.update(existing.id),
+				fakeRequest()
+				.withSession("userId", super.userId)
+				.withFormUrlEncodedBody(ImmutableMap.of(
+						"accountNumber", "1234-1",
+						"accountType", "PG",
+						"iban", "FI0004563215864902",
+						"bic", "NDEAFIHH"
+						))
+				);
+		BankAccount updated = BankAccount.find.where().eq("accountNumber", "1234-1").findUnique();
+		assertNotNull(updated);
+		assertEquals("1234-1", updated.accountNumber);
+
+		assertEquals(303, status(update));
+		assertEquals("/accounts/"+existing.id, header("Location", update));
 	}
-	
+
 	@Test
 	public void testDestroy() {
-		
 	}
 }
